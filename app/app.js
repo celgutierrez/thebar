61ff66db249bc59fc767147391278dbd54464ab0
celgutierrez <celgutierrez42@gmail.com>
2017-06-13 13:42:39 -0700

    added row/col/size and add'l attributes to style

--- app/app.js
+++ app/app.js
@@ -1,30 +1,45 @@
-angular.module('Bar', ['BarCtrls', 'ui.router'])
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/404');
+var app = angular.module('Bar', ['ui.router', 'BarCtrls']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/');
 
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'views/home.html'
-            })
-            .state('about', {
-                url: '/about',
-                templateUrl: 'views/gallery.html'
-            })
-            .state('events', {
-                url: '/events',
-                templateUrl: 'views/events.html'
-            })
-            .state('contact', {
-                url: '/contact',
-                templateUrl: 'views/contact.html'
-            })
-            .state('press', {
-                url: '/press',
-                templateUrl: 'views/press.html'
+                templateUrl: 'views/home.html',
             })
             .state('gallery', {
                 url: '/gallery',
-                templateUrl: 'views/gallery.html'
+                templateUrl: 'views/gallery.html',
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'views/contact.html',
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html',
+            })
+            .state('press', {
+                url: '/press',
+                templateUrl: 'views/press.html',
+            })
+            .state('events', {
+                url: '/events',
+                templateUrl: 'views/events.html',
+            })
+            .state('404', {
+                url: '/404',
+                templateUrl: 'views/404.html'
             });
-    }]);
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    }
+]);
